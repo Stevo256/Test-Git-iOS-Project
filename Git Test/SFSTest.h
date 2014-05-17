43a53b29cfe21f8052705693701efde6ae13bf70
Stephen Sparks <stephensparks@Stephens-Mac-mini.local>
2014-05-17 09:40:07 -0400

    Added test class

---
+++ Git Test/SFSTest.h
@@ -0,0 +1,13 @@
+//
+//  SFSTest.h
+//  Git Test
+//
+//  Created by Stephen Sparks on 2014-05-17.
+//  Copyright (c) 2014 SFS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFSTest : NSObject
+
+@end
---
+++ Git Test/SFSTest.m
@@ -0,0 +1,13 @@
+//
+//  SFSTest.m
+//  Git Test
+//
+//  Created by Stephen Sparks on 2014-05-17.
+//  Copyright (c) 2014 SFS. All rights reserved.
+//
+
+#import "SFSTest.h"
+
+@implementation SFSTest
+
+@end
